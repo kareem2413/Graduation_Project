dbd9534515242e77e9ed35e17931ad1a59e24ced
Kareem2413 <kareem_ahmed2413@yahoo.com>
2019-05-05 05:31:15 -0700

    Initial commit

---
+++ Assets/Scripts/scenechange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void change ()
+	{
+		SceneManager.LoadScene(1);
+	}
+}
